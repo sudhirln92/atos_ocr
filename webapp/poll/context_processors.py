d090a70119c06d7aa7030247611cfffb45313e51
sudhirln92 <sudhirln92@gmail.com>
2023-05-27 09:54:40 +0530

    clean

--- webapp/poll/context_processors.py
+++
@@ -1,5 +0,0 @@
-from poll.models import Question
-
-def polls_count(request):
-    count = Question.objects.count()
-    return {"polls_count":count}
--- webapp/poll/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from poll import views
-
-urlpatterns = [
-    path('', views.index, name='poll_list'),
-    path('<int:id>/details/', views.details, name='poll_details'),
-    path('<int:id>/',views.poll,name='single_poll')
-    
-]
